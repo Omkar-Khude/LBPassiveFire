d43f8c7819f24f7295c16e33826dbcd14dd0abbc
wtomkarkhude <omkarkhude1@gmail.com>
2024-03-13 20:35:20 +0530

    committed

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Table
 @Entity
